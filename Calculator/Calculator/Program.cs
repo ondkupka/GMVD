a91735ee93897695682736f1fa8fef9184cc37e9
Ondrej Kupka <ondkupka@yahoo.com>
2023-09-19 11:27:11 +0200

    Přidány goniometrické funkce do kalkulačky

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -16,10 +16,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Ahoj, s čím ti můžu pomoct?");
+            Console.WriteLine("Zvol 1 pro základní číselné operace, 2 pro výpočet goneometrických funkcí, 3 pro mocnění čísel");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Console.Clear();
+                algebra();
+            }
+            else if (answer == "2")
+            {
+                Console.Clear();
+                goniometrie();
+            }
+            else if (answer == "3")
+            {
+                Console.Clear();
+            }
+            Console.ReadKey();        
+        }
+        public static void algebra()
+        {
             float x, y;
             char no; // no = Numeric Operator 
             float result = 0;
+
 
             Console.WriteLine("Zadej první číslo");
             while (!float.TryParse(Console.ReadLine(), out x))
@@ -38,7 +59,6 @@ namespace Calculator
             {
                 Console.WriteLine("Zadej prosím platnou operaci:");
             }
-            
 
             switch (no) // tohle by šlo udělat i přes if a else if viz zadání, ale tohle je asi jednodušší varianta.
             {
@@ -52,7 +72,7 @@ namespace Calculator
                     result = x * y;
                     break;
                 case '/':
-                    if (y != 0) 
+                    if (y != 0)
                     {
                         result = x / y;
                     }
@@ -64,9 +84,42 @@ namespace Calculator
                     break;
             }
             Console.WriteLine(result);
-            Console.ReadKey();        
-
-            //Ondřej Kupka 4.D 
+        }
+        public static void goniometrie()
+        {
+            char o;
+            double degrees;
+            double radians;
+            double result = 0;
+            Console.WriteLine("Zadej počet stupňů");
+            while (!double.TryParse(Console.ReadLine(), out degrees))
+            {
+                Console.WriteLine("Zadej prosím platné stupně:");
+            }
+            Console.Clear();
+            radians = degrees * Math.PI / 180; //převádíme ze stupňů na radiány 
+            Console.WriteLine("1 pro sinus, 2 pro cosinus, 3 pro tangens, 4 pro cotangens");
+            while (!char.TryParse(Console.ReadLine(), out o) || (o != '1' && o != '2' && o != '3' && o != '4'))
+            {
+                Console.WriteLine("Zadej prosím platnou operaci:");
+            }
+            Console.Clear();
+                switch (o)
+                {
+                    case '1':
+                        result = Math.Sin(radians);
+                        break;
+                    case '2':
+                        result = Math.Cos(radians);
+                        break;
+                    case '3':
+                        result = Math.Tan(radians);
+                        break;
+                    case '4':
+                        result = 1 / Math.Tan(radians);
+                        break;
+                }
+                Console.WriteLine(result);
         }
     }
 }
